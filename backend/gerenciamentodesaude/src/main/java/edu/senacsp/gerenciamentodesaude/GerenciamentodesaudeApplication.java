d09f3341986b9f4d3f85ed7034c0143ab4f50656
André Rodrigues <oandredev@gmail.com>
2026-09-01 20:10:57 -0300

    feat: add project base (backend)

---
+++ backend/gerenciamentodesaude/src/main/java/edu/senacsp/gerenciamentodesaude/GerenciamentodesaudeApplication.java
@@ -0,0 +1,13 @@
+package edu.senacsp.gerenciamentodesaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentodesaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentodesaudeApplication.class, args);
+	}
+
+}
---
+++ backend/gerenciamentodesaude/src/test/java/edu/senacsp/gerenciamentodesaude/GerenciamentodesaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.senacsp.gerenciamentodesaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentodesaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
